32ae4bd799a275e9c8d4998d9e6a389acb6ed5ca
beckheng <beckheng@gmail.com>
2014-10-21 21:05:09 +0800

    修复升级到xcode 6后的编译错误

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -4,6 +4,18 @@
 
 USING_NS_CC;
 
+extern"C"{
+	size_t fwrite$UNIX2003( const void *a, size_t b, size_t c, FILE *d )
+	{
+		return fwrite(a, b, c, d);
+	}
+	
+	char* strerror$UNIX2003( int errnum )
+	{
+		return strerror(errnum);
+	}
+}
+
 AppDelegate::AppDelegate() {}
 
 AppDelegate::~AppDelegate() 
